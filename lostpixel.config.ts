d13128aa0ef04cb1bdc0c6d44b8103576d4b1e47
DevanshAryan <134709519+DevanshAryan@users.noreply.github.com>
2023-06-06 14:16:58 +0530

    Update lostpixel.config.ts

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,9 +1,12 @@
 import { CustomProjectConfig } from 'lost-pixel';
-export const config: CustomProjectConfig = {
-  storybookShots: {
-    storybookUrl: './storybook-static',
-  },
-  generateOnly: true,
-  failOnDifference: true,
-};
 
+ 
+
+    export const config: CustomProjectConfig = {
+      storybookShots: {
+        storybookUrl: './storybook-static',
+      },
+
+      lostPixelProjectId: 'clijvkulu8q3kn70ei9xugtyv',
+      apiKey: process.env.LOST_PIXEL_API_KEY,
+    };
